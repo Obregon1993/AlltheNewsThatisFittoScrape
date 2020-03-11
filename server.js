a3e271392dfc9b19f118df09fd28d3ed0d9b4f28
Obregon1993 <cjangie13@gmail.com>
2020-03-10 23:03:58 -0400

    donde

---
+++ server.js
@@ -0,0 +1,36 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+
+var express = require('express');
+var app = express();
+
+app.use(logger('dev'));
+app.use(
+        bodyParser.urlencoded({
+            extended: false
+        })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// 
+mongoose.connect("mongodb://localhost/AllTheNews", { useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function(){
+    console.log("Connected to mongoose");
+});
+
+var routes = require("./controller/controller");
+app.use("/", routes);
+
+var PORT = process.env.PORT || 3000; 
+app.listen(PORT, function(){
+    console.log('listening on PORT ' + PORT);
+});
